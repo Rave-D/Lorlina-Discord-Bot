4cc1462d3c9f5ece7f846ae5342056be1e801227
Rave D <raved.dev@gmail.com>
2018-11-13 12:44:50 +0100

    [Test] Add sandbox project to test bot.

---
+++ LorlinaBot.Sandbox/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LorlinaBot.Sandbox
+{
+    using System;
+    using System.Threading.Tasks;
+    using LorlinaBot.Discord;
+
+    class Program
+    {
+        private const string DISCORDBOT_TOKEN = "INSERT TOKEN HERE";
+
+        public static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            await DiscordClient.Instance.StartClientAsync(DISCORDBOT_TOKEN).ConfigureAwait(false);
+
+            // Block the program until it is closed.
+            await Task.Delay(-1).ConfigureAwait(false);
+        }
+    }
+}
